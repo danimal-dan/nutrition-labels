6150a3b234e25e70cca50eb3618dbe90c88c52cf
Daniel Collins <danielc111@gmail.com>
2024-03-18 22:06:15 -0400

    Add 4224 template

--- lambda/AveryLabels.py
+++ lambda/AveryLabels.py
@@ -26,6 +26,8 @@ from reportlab.pdfbase.ttfonts import TTFont
 # page margins left/top
 
 labelInfo = {
+    # 2.375 x 1.25 food labels
+    4224: ( 3, 6, (163,  78), (30, 30), (28, 63)),
     # 2.6 x 1 address labels
     5160: ( 3, 10, (187,  72), (11, 0), (18, 46)),
     5161: ( 2, 10, (288,  72), (0, 0), (18, 36)),
