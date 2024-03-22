178b291985345019641e3e891744952143b9b101
Daniel Collins <danielc111@gmail.com>
2024-03-21 23:04:43 -0400

    Getting pretty close

--- .ci/create-stack.sh
+++ .ci/create-stack.sh
@@ -4,17 +4,32 @@
 # temp to generate a unique name
 CODEPIPELINE_STACK_NAME="nutrition-labels-$(date +%Y-%m-%d-%H-%M-%S)"
 
-if [ -z ${1} ]
+githubAuthToken=""
+
+# Loop through arguments and process them
+for arg in "$@"
+do
+    case $arg in
+        --github-auth-token=*)
+        githubAuthToken="${arg#*=}"
+        shift # Remove processed argument
+        ;;
+        *)    # Unknown option
+        ;;
+    esac
+done
+
+if [ -z ${githubAuthToken} ]
 then
 	echo "PIPELINE CREATION FAILED!"
-        echo "Pass your Github OAuth token as the first argument"
+        echo "--github-auth-token argument is required"
 	exit 1
 fi
 
 aws cloudformation create-stack \
         --capabilities CAPABILITY_IAM \
         --stack-name $CODEPIPELINE_STACK_NAME \
-        --parameters ParameterKey=GitHubOAuthToken,ParameterValue=${1} \
+        --parameters ParameterKey=GitHubOAuthToken,ParameterValue=${githubAuthToken} \
         --template-body file://$(dirname "$0")/001-cloud-formation-infra-and-codebuild.yaml \
         --region us-east-1 \
         --profile default
